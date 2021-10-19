02e92029b93da6f6fc31ff249142c2124493da52
Roman Korostenskyi <romankorostensky@gmail.com>
2021-10-19 12:37:26 +0300

    feature: sentry

--- app/src/main/java/io/korostenskyi/chestnut/App.kt
+++ app/src/main/java/io/korostenskyi/chestnut/App.kt
@@ -2,6 +2,23 @@ package io.korostenskyi.chestnut
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.korostenskyi.chestnut.domain.model.BuildParams
+import io.sentry.Sentry
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    @Inject lateinit var buildParams: BuildParams
+
+    override fun onCreate() {
+        super.onCreate()
+        initSentry()
+    }
+
+    private fun initSentry() {
+        if (buildParams.isSentryEnabled) {
+            Sentry.init(buildParams.sentryUrl)
+        }
+    }
+}
